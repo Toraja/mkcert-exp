9bdbc8631d527bd2586076733b9629f1eb7736e0
Toraja <toraja.blend.french@gmail.com>
2021-08-21 02:20:22 +0900

    implement app server

--- app/main.go
+++ app/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("ok")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", greet)
+	http.ListenAndServe(":8080", mux)
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	resp, _ := json.Marshal(response{Greeting: "How are you?"})
+	fmt.Fprint(w, string(resp))
+}
+
+type response struct {
+	Greeting string `json:"greeting"`
 }
